2085632fe8feb15ccd4f0b8e41497d8f5424c37a
cardremi <syuaibdhedy@gmail.com>
2021-01-13 12:38:54 +0700

    Header Nav

--- Src/Shared/Navigation.js
+++ Src/Shared/Navigation.js
@@ -4,13 +4,19 @@ import {createStackNavigator} from '@react-navigation/stack'
 import Home from '../Feature/Home/Home'
 import About from '../Feature/About/About'
 
+
 const Stack = createStackNavigator ()
 
 export default function Navigation() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home}/>
-            <Stack.Screen name="About" component={About}/>
+            <Stack.Screen name="Home" component={Home} options={{
+                headerShown:false,
+                
+            }}/>
+            <Stack.Screen name="About" component={About} options={{
+                headerShown:false,
+            }}/>
         </Stack.Navigator>
     )
 }
